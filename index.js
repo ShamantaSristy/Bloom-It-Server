3f5bb5217eb819d1174034f764e62abae283ea10
Shamanta Sristy <shamantasristy@gmail.com>
2021-04-02 22:08:10 +0600

    done

--- index.js
+++ index.js
@@ -24,8 +24,31 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 client.connect(err => {
-    console.log('connection error', err);
+    // console.log('connection error', err);
   const orderCollection = client.db("bloomitdb").collection("orders");
+  const productCollection = client.db("bloomitdb").collection("products");
+
+
+  app.post('/addProduct', (req, res) => {
+    const newProduct = req.body;
+    console.log('adding new product ', newProduct);
+    productCollection.insertOne(newProduct)
+    .then(result => {
+        // console.log('inserted count ',result.insertedCount);
+        res.send(result.insertedCount > 0)
+    })
+})
+
+app.get('/products', (req, res) => {
+  // console.log('working');
+  productCollection.find()
+  .toArray((err, items) => {
+      // console.log(err);
+      // console.log('working 2');
+      res.send(items)
+      // console.log('from database',items);
+  })
+})
 
     app.get('/orders', (req, res) => {
         // console.log('working');
@@ -39,9 +62,9 @@ client.connect(err => {
     })
 
   app.post('/addOrder', (req, res) => {
-      const newProduct = req.body;
-      console.log('adding new product ', newProduct);
-      orderCollection.insertOne(newProduct)
+      const newOrder = req.body;
+      console.log('adding new product ', newOrder);
+      orderCollection.insertOne(newOrder)
       .then(result => {
           console.log('inserted count ',result.insertedCount);
           res.send(result.insertedCount > 0)
@@ -49,12 +72,24 @@ client.connect(err => {
   })
 
 
-  app.delete('deleteItem/:id', (req, res) => {
-    const id = ObjectID(req.params.id);
-    console.log('delete item', id);
-    orderCollection.findOneAndDelete({_id: id})
-    .then(documents => res.send(!!documents.value))
-  })
+  app.delete('/deleteItem/:id',(req,res) => {
+    const Id = ObjectID(req.params.id);
+    console.log("delete this",  Id);
+    productCollection.deleteOne({_id: Id})
+    .then(documents => {
+      console.log(documents.deletedCount);
+      res.send(documents)
+    })
+  });
+
+
+
+  // app.delete('deleteItem/:id', (req, res) => {
+  //   const id = ObjectID(req.params.id);
+  //   console.log('delete item', id);
+  //   orderCollection.findOneAndDelete({_id: id})
+  //   .then(documents => res.send(!!documents.value))
+  // })
 //   client.close();
 });
 
